7c18dd20f5b5e945160c78683b7fc734da366284
SeeGreatHu <hxg5211217@qq.com>
2022-09-14 21:08:42 +0800

    完成eachart

---
+++ src/components/report/Report.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片区域 -->
+    <el-card>
+      <div id="main" style="width: 600px; height: 400px"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  name: 'MyReport',
+  components: {},
+  props: {},
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    const myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message({
+        message: '获取数据失败！',
+        type: 'error'
+      })
+    }
+    const result = _.merge(res.data, this.options)
+    // 绘制图表
+    myChart.setOption(result)
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
